f81a346827c7869c9ec227fd03ea3303f74002f2
Philipp Kief <philipp.kief@gmx.de>
2025-05-28 17:30:22 +0200

    Sync the icons from the VS Code Material Icon Theme (#26)
    Hi, I'm the maintainer of the [VS Code Material Icon
    Theme](https://github.com/material-extensions/vscode-material-icon-theme).
    While playing around with Zed I also wanted to know how we could easily
    sync the Material Icons of VS Code with this extension from Zed.
    Synching the icons will be way more comfortable than letting the users
    open issues and PRs for every single icon (which we have been going
    through for the VS Code extension already).
    
    That's why I came up with a small script that loads the Material Icons
    from our [npm
    package](https://www.npmjs.com/package/material-icon-theme). The npm
    package also provides a function `generateManifest` which will generate
    a JSON file with all the icon to file name mappings. The script
    generates this JSON file and maps the structure of it to the API of Zed.
    
    The script command (uses Node.js and npm):
    
    ```
    npm run build
    ```
    
    Generates the following JSON file:
    
    https://github.com/PKief/zed-material-icon-theme/blob/69d89b76c12dcc7adc9f2f01ced44b7b0f658104/icon_themes/material-icon-theme.json
    
    The outcome looks like this:
    
    <img width="369" alt="image"
    src="https://github.com/user-attachments/assets/f5753e9b-7a02-4ab2-bddc-3d78901c446f"
    />
     
    I'm aware of the fact, that Zed is rather based on Rust than on Node.js.
    However, if you like to have a solution where we "sync" these icons, I
    could also imagine that we create a GitHub Action which will create a
    sync update for this extension everytime when we push a new version to
    the npm package.
    
    If you don't see a sync solution at all, it would be still better to run
    that script at least once to provide all icons at once and keep going
    with it independently. Just to safe some time spending with creating new
    icons and all these things. I'd be happy to support here.
    
    Related to #25
    
    ---------
    
    Co-authored-by: Marshall Bowers <git@maxdeviant.com>

---
+++ src/build.ts
@@ -0,0 +1,40 @@
+import { writeFileSync } from "node:fs";
+import { generateManifest } from "material-icon-theme";
+import { join } from "node:path";
+import { getTheme } from "./theme";
+import fs from "node:fs";
+
+const manifest = generateManifest();
+const zedIconTheme = getTheme(manifest);
+
+const zedManifest = {
+  $schema: "https://zed.dev/schema/icon_themes/v0.2.0.json",
+  name: "Material Icon Theme",
+  author: "Zed Industries",
+  themes: [zedIconTheme],
+};
+
+writeFileSync(
+  join(__dirname, "../icon_themes", "material-icon-theme.json"),
+  JSON.stringify(zedManifest, null, 2),
+);
+
+// Copy icons from node_modules to the icons directory
+const iconsSourceDir = join(
+  __dirname,
+  "../node_modules/material-icon-theme/icons",
+);
+const iconsDestDir = join(__dirname, "../icons");
+if (!fs.existsSync(iconsDestDir)) {
+  fs.mkdirSync(iconsDestDir, { recursive: true });
+}
+fs.readdirSync(iconsSourceDir).forEach((file) => {
+  // Skip files that start with "folder-" except for "folder-open.svg"
+  if (file.startsWith("folder-") && file !== "folder-open.svg") {
+    return;
+  }
+  const sourceFile = join(iconsSourceDir, file);
+  const destFile = join(iconsDestDir, file);
+  fs.copyFileSync(sourceFile, destFile);
+});
+console.log("Material Icon Theme icons copied successfuly.");
